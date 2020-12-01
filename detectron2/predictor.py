ddb64df2433d75b988f94387f9c8a73833010301
Sriram Muthaiah <srirambtecit@gmail.com>
2020-12-01 16:30:07 +0000

    update code

--- detectron2/predictor.py
+++ detectron2/predictor.py
@@ -44,17 +44,19 @@ class VisualizationDemo(object):
           frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
           vis_frame = None
           if "instances" in predictions:
-            instances = predictions["instances"]
-            # pred_classes = instances[ instances.pred_classes == 2].pred_classes
-            pred_classes = instances.pred_classes
-            # only interested in frame classified as "umpire"
-            if 2 in pred_classes:
-              # print('Pred_classes', pred_classes)
-              predictions = instances.to(self.cpu_device)
-              vis_frame = self.umpire_classifier.video_visualizer().draw_instance_predictions(frame, predictions)
+            predictions = instances.to(self.cpu_device)
+            vis_frame = self.umpire_classifier.video_visualizer().draw_instance_predictions(frame, predictions)
+            # Converts Matplotlib RGB format to OpenCV BGR format
+            vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
 
-              # Converts Matplotlib RGB format to OpenCV BGR format
-              vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
+            # instances = predictions["instances"]
+            # # only interested in frame classified as "umpire"
+            # if 2 in instances.pred_classes:
+            #   # print('Pred_classes', pred_classes)
+            #   predictions = instances.to(self.cpu_device)
+            #   vis_frame = self.umpire_classifier.video_visualizer().draw_instance_predictions(frame, predictions)
+            #   # Converts Matplotlib RGB format to OpenCV BGR format
+            #   vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
           return vis_frame
 
         frame_gen = self._frame_from_video(video)
