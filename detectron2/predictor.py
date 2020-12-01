fd48dbde31925889a8e91f2637d5c2fe4e1789ee
Sriram Muthaiah <srirambtecit@gmail.com>
2020-12-01 17:16:40 +0000

    udpate prediction logic by combining two models

--- detectron2/predictor.py
+++ detectron2/predictor.py
@@ -10,12 +10,13 @@ from detectron2.engine.defaults import DefaultPredictor
 from detectron2.utils.video_visualizer import VideoVisualizer
 from detectron2.utils.visualizer import ColorMode, Visualizer
 
+
 class VisualizationDemo(object):
     def __init__(self, args, cfg, instance_mode=ColorMode.IMAGE):
         if args.classifier == 'umpire-classifier':
-          self.umpire_classifier = UmpireClassifier(cfg, instance_mode)
+            self.umpire_classifier = UmpireClassifier(cfg, instance_mode)
         elif args.classifier == 'umpire-pose-classifier':
-          self.umpire_signs_classifier = UmpireSignsClassifier(cfg, instance_mode)
+            self.umpire_signs_classifier = UmpireSignsClassifier(cfg, instance_mode)
         self.cpu_device = torch.device("cpu")
 
     def _frame_from_video(self, video):
@@ -37,34 +38,35 @@ class VisualizationDemo(object):
         Yields:
             ndarray: BGR visualizations of each video frame.
         """
-        def process_umpire_signs_predictions(frame, predictions):
-          pass
-
         def process_predictions(frame, predictions):
-          frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-          vis_frame = None
-          if "instances" in predictions:
-            instances = predictions["instances"]
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            vis_frame = None
+            video_visualizer = self.umpire_signs_classifier.video_visualizer()
 
-            # getting all frames
-            predictions = instances.to(self.cpu_device)
-            vis_frame = self.umpire_classifier.video_visualizer().draw_instance_predictions(frame, predictions)
-            # Converts Matplotlib RGB format to OpenCV BGR format
-            vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
+            if "instances" in predictions:
+                instances = predictions["instances"]
 
-            # # only interested in frame classified as "umpire"
-            # if 2 in instances.pred_classes:
-            #   # print('Pred_classes', pred_classes)
-            #   predictions = instances.to(self.cpu_device)
-            #   vis_frame = self.umpire_classifier.video_visualizer().draw_instance_predictions(frame, predictions)
-            #   # Converts Matplotlib RGB format to OpenCV BGR format
-            #   vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
-          return vis_frame
+                # # getting all frames
+                # predictions = instances.to(self.cpu_device)
+                # vis_frame = self.umpire_classifier.video_visualizer().draw_instance_predictions(frame, predictions)
+                # # Converts Matplotlib RGB format to OpenCV BGR format
+                # vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
+
+                # only interested in frame classified as "umpire"
+                if 2 in instances.pred_classes:
+                    # print('Pred_classes', pred_classes)
+                    # predictions = instances.to(self.cpu_device)
+                    # vis_frame = self.umpire_classifier.video_visualizer().draw_instance_predictions(frame, predictions)
+                    vis_frame = self.umpire_signs_classifier.predict_and_draw(frame, video_visualizer)
+                    
+                    # Converts Matplotlib RGB format to OpenCV BGR format
+                    vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
+            return vis_frame
 
         frame_gen = self._frame_from_video(video)
         for frame in frame_gen:
-          yield process_predictions(frame, self.umpire_classifier.predictor(frame))
-            
+            yield process_predictions(frame, self.umpire_classifier.predictor(frame))
+
         # video_visualizer = VideoVisualizer(self.metadata, self.instance_mode)
 
         # def process_predictions(frame, predictions):
@@ -81,31 +83,46 @@ class VisualizationDemo(object):
         # for frame in frame_gen:
         #     yield process_predictions(frame, self.predictor(frame))
 
-class UmpireClassifier(object):
-  def __init__(self, cfg, instance_mode):
-    self.metadata = MetadataCatalog.get(
-        cfg.DATASETS.TEST[0] if len(cfg.DATASETS.TEST) else "__unused"
-    )
-    self.metadata.thing_classes = ['isumpire', 'non-umpire', 'umpire' ]
-    self.metadata.thing_dataset_id_to_contiguous_id=({0: 0, 1: 1, 2: 2})
-    self.predictor = DefaultPredictor(cfg)
-    self.instance_mode = instance_mode
 
-  def video_visualizer(self):
-    return VideoVisualizer(self.metadata, self.instance_mode)
+class UmpireClassifier(object):
+    def __init__(self, cfg, instance_mode):
+        self.metadata = MetadataCatalog.get(
+            cfg.DATASETS.TEST[0] if len(cfg.DATASETS.TEST) else "__unused"
+        )
+        self.metadata.thing_classes = ['isumpire', 'non-umpire', 'umpire']
+        self.metadata.thing_dataset_id_to_contiguous_id = ({0: 0, 1: 1, 2: 2})
+        self.predictor = DefaultPredictor(cfg)
+        self.instance_mode = instance_mode
+
+    def video_visualizer(self):
+        return VideoVisualizer(self.metadata, self.instance_mode)
+
 
 class UmpireSignsClassifier(object):
-  def __init__(self, cfg, instance_mode):
-    self.metadata = MetadataCatalog.get(
-        cfg.DATASETS.TEST[0] if len(cfg.DATASETS.TEST) else "__unused"
-    )
-    self.metadata.thing_classes = ['umpire-signals', 'no-action', 'no-ball', 'out', 'six', 'wide']
-    self.metadata.thing_dataset_id_to_contiguous_id=({0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5})
-    self.predictor = DefaultPredictor(cfg)
-    self.instance_mode = instance_mode
+    def __init__(self, cfg, instance_mode):
+        self.metadata = MetadataCatalog.get(
+            cfg.DATASETS.TEST[0] if len(cfg.DATASETS.TEST) else "__unused"
+        )
+        self.metadata.thing_classes = ['umpire-signals', 'no-action', 'no-ball', 'out', 'six', 'wide']
+        self.metadata.thing_dataset_id_to_contiguous_id = ({0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5})
+        self.predictor = DefaultPredictor(cfg)
+        self.instance_mode = instance_mode
 
-  def video_visualizer(self):
-    return VideoVisualizer(self.metadata, self.instance_mode)
+    def video_visualizer(self):
+        return VideoVisualizer(self.metadata, self.instance_mode)
+
+    def predict_and_draw(frame, viz: VideoVisualizer):
+        predictions = self.predictor(frame)
+
+        if "instances" in predictions:
+            instances = predictions["instances"]
+
+            # only interested in frame not classified as NO-ACTION
+            # such as OUT, SIX, NO-BALL, WIDE
+            if 1 not in instances.pred_classes:
+                # print('Pred_classes', pred_classes)
+                predictions = instances.to(self.cpu_device)
+                vis_frame = viz.draw_instance_predictions(frame, predictions)
 
 """
 sudo code:
